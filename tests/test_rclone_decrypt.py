5b065487fdadf57c2274910d5154c65b6562b6e3
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2023-04-23 22:39:20 -0700

    failing

--- tests/test_rclone_decrypt.py
+++ tests/test_rclone_decrypt.py
@@ -22,14 +22,15 @@ def test_version():
     assert __version__ == '0.1.0'
 
 
-def compare_files():
+def compare_files(encrypted_folder :str):
     file_match_sub_folder = []
     for i in range(0,3):
         original_file = os.path.join(
                 test_dir, 'raw_files', 'sub_folder', f'file{i}.txt')
 
         decrypted_file = os.path.join(
-                default_out_dir, f'encrypted_files{i}', 'sub_folder',
+                default_out_dir, encrypted_folder,
+                'sub_folder',
                 f'file{i}.txt')
 
         file_match_sub_folder.append(filecmp.cmp(original_file, decrypted_file))
@@ -43,14 +44,14 @@ def compare_files():
     return all(file_match_sub_folder) and file_match
 
 
-def decrypt_test(files:str) -> bool:
+def decrypt_test(encrypted_folder :int, files :str) -> bool:
     instance = decrypt.get_rclone_instance(decrypt_rclone_config_file, files)
 
     decrypt.decrypt(instance,
                     files,
                     decrypt.default_output_folder)
 
-    return compare_files()
+    return compare_files(encrypted_folder)
 
 
 def test_encrypted_file0(setup):
@@ -58,8 +59,9 @@ def test_encrypted_file0(setup):
     Test that encrypted files with unencrypted file names and folder names are
     decrypted
     """
-    files = 'tests/encrypted_files0'
-    assert(decrypt_test(files) == True)
+    folder = 'encrypted_files0'
+    files = f'tests/{folder}'
+    assert(decrypt_test(folder, files) == True)
 
 
 def test_encrypted_file1(setup):
@@ -67,8 +69,9 @@ def test_encrypted_file1(setup):
     Test that encrypted files with encrypted file names and unencrypted folder
     names are decrypted
     """
-    files = 'tests/encrypted_files1'
-    assert(decrypt_test(files) == True)
+    folder = 'encrypted_files0'
+    files = f'tests/{folder}'
+    assert(decrypt_test(folder, files) == True)
 
 
 def test_encrypted_file2(setup):
@@ -76,8 +79,9 @@ def test_encrypted_file2(setup):
     Test that encrypted files with encrypted file names and folder
     names are decrypted
     """
-    files = 'tests/0f12hh28evsof1kgflv67ldcngbgfa8j4viad0q5ie7mj1n1m490'
-    assert(decrypt_test(files) == True)
+    folder = '0f12hh28evsof1kgflv67ldcngbgfa8j4viad0q5ie7mj1n1m490'
+    files = f'tests/{folder}'
+    assert(decrypt_test(folder, files) == True)
 
 
 def test_decrypted_files_default_location():
